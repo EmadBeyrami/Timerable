02c9e039e2e98561c6e30790ee574f480a262367
Emad Beyrami <29242915+EmadBeyrami@users.noreply.github.com>
2021-09-29 17:37:51 +0330

    Add files via upload
    Added Timerable

---
+++ RxTimerable/TimeCounter.swift
@@ -0,0 +1,135 @@
+// RxTimeCounter.Swift
+//
+// Designed and Developed By Emad Bayrami & Mohamad NasrAbadi
+
+import UIKit
+import Foundation
+import RxSwift
+import RxCocoa
+
+class STRTimeCounter {
+    
+    private (set) var state: TimerState?
+    
+    // MARK: - Properties
+    private var backgroundTime: Date?
+    private var background_forground_timelaps: Int?
+    private let disposeBag = DisposeBag()
+    private var sourceObservable: Disposable?
+    private var second: Double = 0
+    
+    private var isTimerWorking: Bool {
+        guard state != nil else {
+            print("⏰ you should call 'START()' First!")
+            return false
+        }
+        return true
+    }
+    
+    var stringTime: TimeModel {
+        let timeBuilder = TimeBuilder()
+        return timeBuilder.getTimeString(second)
+    }
+    
+    // MARK: - LifeCycle
+    
+    // initializer
+    private var time: Timerable!
+    func setTimer(time: Timerable) {
+        self.time = time
+        self.second = time.time.seconds
+    }
+    
+    // deinitializer
+    deinit {
+        sourceObservable?.dispose()
+        print("REMOVED \(self) FROM MEMORY")
+        print(time.description)
+    }
+    
+    // MARK: - Timer Helper
+    fileprivate func checkTheDifference() {
+        guard let BGAFTimelapse = background_forground_timelaps else { return }
+        guard backgroundTime == nil else { return }
+        switch time.timerType {
+        case .decreasing:
+            self.second -= Double(BGAFTimelapse)
+        case .increasing:
+            self.second += Double(BGAFTimelapse)
+        }
+        background_forground_timelaps = nil
+    }
+    
+    // MARK: - Bindings and actions
+    fileprivate func appStateBinding() {
+        UIApplication.rx.willEnterForeground
+            .subscribe({ [weak self] _ in
+                guard let self = self else { return }
+                guard let date = self.backgroundTime else { return }
+                let component = Calendar.current.dateComponents([.second], from: date, to: Date())
+                self.background_forground_timelaps = component.second
+                self.backgroundTime = nil
+            }).disposed(by: disposeBag)
+        
+        UIApplication.rx.didEnterBackground
+            .subscribe({ [weak self] _ in
+                guard let self = self else { return }
+                self.background_forground_timelaps = nil
+                self.backgroundTime = Date()
+            }).disposed(by: disposeBag)
+    }
+    
+    private var timeEnded: Completion = { }
+    func timerDidEnd(timeEnded: @escaping Completion) {
+        self.timeEnded = timeEnded
+    }
+    
+    private var logTime: DataCompletion<TimeModel> = { _ in }
+    func bind(_ log: @escaping DataCompletion<TimeModel>) {
+        logTime = log
+    }
+    
+    // MARK: - Timer Logic
+    
+    // Timer Start
+    func start() {
+        guard state == nil else { return }
+        state = .working
+        appStateBinding()
+        
+        sourceObservable = Observable<Int>
+            .interval(RxTimeInterval.seconds(time.repeatTime), scheduler: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] _ in
+                guard let self = self else { return }
+                self.checkTheDifference()
+                self.updateTime()
+            })
+    }
+    
+    private func timeDone() {
+        timeEnded()
+        sourceObservable?.dispose()
+    }
+    
+    // Update Time Logic
+    fileprivate func updateTime() {
+        
+        switch time.timerType {
+        case .increasing:
+            if second >= time.endTime.seconds {
+                timeDone()
+                return
+            }
+            second += time.jumpInterval
+            
+        case .decreasing:
+            if second <= time.endTime.seconds || second <= 0 {
+                timeDone()
+                return
+            }
+            second -= time.jumpInterval
+        }
+        logTime(stringTime)
+    }
+    
+}
---
+++ RxTimerable/Timerable.swift
@@ -0,0 +1,68 @@
+// RxTimeCounter.Swift
+//
+// Designed and Developed By Emad Bayrami & Mohamad NasrAbadi
+
+import Foundation
+
+protocol Timerable {
+    var time: [TimeIntervalType] { get }
+    var timerType: TimerType { get }
+    var repeatTime: Int { get }
+    var jumpInterval: Double { get }
+    var endTime: [TimeIntervalType] { get }
+}
+
+extension Timerable {
+    var repeatTime: Int { 1 }
+    var jumpTime: Int { 1 }
+}
+
+extension Timerable {
+    var description: String {
+        return """
+        time: \(time)
+        timerType: \(timerType)
+        repeatTime: \(repeatTime)
+        jumpTime: \(jumpInterval)
+        endTime: \(endTime)
+        ⏲
+        """
+    }
+}
+
+enum TimeIntervalType {
+    case days(Double)
+    case hours(Double)
+    case minutes(Double)
+    case seconds(Double)
+    
+    var second: Double {
+        switch self {
+        case .days(let day):
+            return (day * 24) * 60 * 60
+        case .hours(let hour):
+            return (hour * 60) * 60
+        case .minutes(let minute):
+            return (minute * 60)
+        case .seconds(let second):
+            return second
+        }
+    }
+}
+
+enum TimerState {
+    case working
+    case resumed
+    case suspended
+}
+
+enum TimerType {
+    case increasing
+    case decreasing
+}
+
+extension Sequence where Self == [TimeIntervalType] {
+    var seconds: Double {
+        self.compactMap({ $0.second }).reduce(0, +)
+    }
+}
